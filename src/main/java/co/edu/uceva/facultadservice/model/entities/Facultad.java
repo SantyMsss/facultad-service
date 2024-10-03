1804412afaf4be99589a6daf4e43deef667f08ec
Felipe Marulanda <juan.castro05@uceva.edu.co>
2024-10-03 00:37:53 -0500

    Llenado de entidad facultad realizado

--- src/main/java/co/edu/uceva/facultadservice/model/entities/Facultad.java
+++ src/main/java/co/edu/uceva/facultadservice/model/entities/Facultad.java
@@ -1,4 +1,24 @@
 package co.edu.uceva.facultadservice.model.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
 public class Facultad {
+    @Id
+    private String nombreFacu;
+    private String codigoFacu;
+    private String decano;
+    private String modalidad;
+    private String proyecInvestFacu;
+    private String descripcion;
+    private String fechaCrea;
+    private String telefono;
+    private String correo;
+    private String programasAcademicos;
+    private String calendarioAcademico;
 }
