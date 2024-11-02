050bd557ad4436dc98c48b06b16101f5ec3c49de
SantyMss <santiagomarse@hotmail.com>
2024-11-01 20:47:06 -0500

    fix code

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,13 +1,1 @@
-INSERT INTO facultad (
-    nombre_facu,
-    decano,
-    modalidad,
-    proyec_invest_facu,
-    descripcion,
-    fecha_crea,
-    telefono,
-    correo,
-    prog_academico,
-    calendar_academico
-) VALUES
-      ('Facultad de Ciencias Ambientales', 'Dr. Pedro Rojas', 'Presencial', 'Gestion Ambiental y Cambio Climatico', 'Facultad enfocada en la proteccion del medio ambiente y la sostenibilidad.', CURRENT_TIMESTAMP, 889900112, 'ambientales@ejemplo.com', 'Ingenieria Ambiental, Ciencias Ambientales', '2024-2025');
+INSERT INTO facultad (nombre_facu,decano,modalidad,proyec_invest_facu,descripcion,fecha_crea,telefono,correo,prog_academico,calendar_academico) VALUES('Facultad de Ciencias Ambientales','Dr. Pedro Rojas','Presencial','Gestion Ambiental y Cambio Climatico','Facultad enfocada en la proteccion del medio ambiente y la sostenibilidad.',CURRENT_TIMESTAMP,889900112,'ambientales@ejemplo.com','Ingenieria Ambiental, Ciencias Ambientales','2024-2025');
