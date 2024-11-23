9be6f54841a0077beafdea5cdc12e5b68323de12
SantyMss <santiagomarse@hotmail.com>
2024-11-22 21:36:52 -0500

    cross origin

--- src/main/java/co/edu/uceva/facultadservice/controller/FacultadRestController.java
+++ src/main/java/co/edu/uceva/facultadservice/controller/FacultadRestController.java
@@ -14,6 +14,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/facultad-service")
+@CrossOrigin(origins =  "http://localhost:4200")
 public class FacultadRestController {
 
     @Autowired
